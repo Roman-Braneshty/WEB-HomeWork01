122a1ba6e6c756f73bfa9742a868a83f065c9f80
Roman-Braneshty <102828727+Roman-Braneshty@users.noreply.github.com>
2022-07-13 23:05:46 +0300

    UPD

--- homework01.py
+++ homework01.py
@@ -1,96 +1,60 @@
-from abc import ABCMeta, abstractmethod
 import json
-import pickle
-from my_logger import get_logger
+from abc import ABC, abstractmethod
 
-logger = get_logger(__name__)
 
-# 1
-
-class SerializationInterface(metaclass=ABCMeta):
-    def __init__(self, data, filename):
-        self.data = data
-        self.__filename = None
-        self.filename = filename
-        logger.info(f'{self.filename}')
-
+class SerializationInterface(ABC):
     @abstractmethod
-    def serialize(self):
-        pass
-
-    @abstractmethod
-    def deserialize(self):
+    def serialize(self, file_name, data):
         pass
 
 
-class SerializeJson(SerializationInterface):
-    @property
-    def filename(self):
-        return self.__filename
+class SerialToJson(SerializationInterface):
+    def serialize(self, file_name, data_json):
+        with open(file_name, 'w', encoding='utf-8') as fh:
+            json.dump(data_json, fh)
 
-    @filename.setter
-    def filename(self, filename):
-        parse = filename.split('.')
-        if parse[1] == 'json':
-            self.__filename = filename
-        else:
-            logger.warning(f'{ValueError}')
-            raise ValueError("File must be JSON format")
-
-    def serialize(self):
-        with open(self.__filename, 'w') as file:
-            json.dump(self.data, file, ensure_ascii=False)
-            logger.info(f'{self.data}')
-
-    def deserialize(self):
-        with open(self.__filename, 'r') as file:
-            unpacked = json.load(file)
-            print(unpacked)
-            logger.debug(f'{unpacked}')
+    def deserialize(self, file_name):
+        with open(file_name, 'r', encoding='utf-8') as fh:
+            return json.load(fh)
 
 
-class SerializeBin(SerializationInterface):
-    @property
-    def filename(self):
-        return self.__filename
+class SerialToBin(SerializationInterface):
+    def serialize(self, file_name, data):
+        with open(file_name, 'wb') as fh:
+            fh.write(data.encode())
 
-    @filename.setter
-    def filename(self, filename):
-        parse = filename.split('.')
-        if parse[1] == 'bin':
-            self.__filename = filename
-        else:
-            raise ValueError("File must be BIN format")
+    def deserialize(self, file_name):
+        with open(file_name, 'rb') as fh:
+            return fh.read().decode()
 
-    def serialize(self):
-        with open(self.__filename, 'wb') as file:
-            pickle.dump(self.data, file)
 
-    def deserialize(self):
-        with open(self.__filename, 'rb') as file:
-            unpacked = pickle.load(file)
-            print(unpacked)
+data_test = 'Python Web 6'
+
+instance_bin = SerialToBin()
+instance_json = SerialToJson()
+
+
+instance_bin.serialize('test.bin', data_test)
+instance_json.serialize('test.json', data_test)
+
+print(instance_bin.deserialize('test.bin'))
+print(instance_json.deserialize('test.json'))
 
 
 # 2
 
+
 class Meta(type):
     class_number = 0
-    children_number = 0
-    logger.debug(f'{children_number, class_number}')
 
-    def __new__(mcs, *args):
-        instance = type.__new__(mcs, *args)
-        if mcs.children_number == 0:
-            mcs.class_number = 0
+    def __new__(mcs, name, bases, namespace, **kwargs):
+        instance = super().__new__(mcs, name, bases, namespace, **kwargs)
         instance.class_number = mcs.class_number
         mcs.class_number += 1
-        mcs.children_number += 1
-        logger.info(f'{instance}')
         return instance
 
 
-Meta.children_number = 0
+Meta.children_number = 5
 
 
 class Cls1(metaclass=Meta):
@@ -107,12 +71,3 @@ if __name__ == '__main__':
     assert (Cls1.class_number, Cls2.class_number) == (0, 1)
     a, b = Cls1(''), Cls2('')
     assert (a.class_number, b.class_number) == (0, 1)
-    test_data = {'name': 'Roman', 'phone': ['0673566185'], 'age': 19}
-
-    json_test = SerializeJson(test_data, 'DB.json')
-    json_test.serialize()
-    json_test.deserialize()
-
-    bin_test = SerializeBin(test_data, 'DB.bin')
-    bin_test.serialize()
-    bin_test.deserialize()
